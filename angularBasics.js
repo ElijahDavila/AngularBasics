8b18ef974774215b7a6d25ec1071eaf6f2157332
ElijahDavila <elijahdavila96@gmail.com>
2020-05-26 23:02:32 -0400

    Angular basics lab

---
+++ angularBasics.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return "Hello, " + person;
+}
+var user = "Super Student";
+document.getElementById("para").innerHTML = sayHello(user);
---
+++ angularBasics.ts
@@ -0,0 +1,15 @@
+function sayHello(person: string): string {
+    return "Hello, " + person;
+   }
+   var user = "Super Student";
+   document.getElementById("para").innerHTML = sayHello(user);
+
+   class person {
+        firstName: String;
+        lastName: String;
+        age: number;
+        phoneNumber: number;
+        state: String;
+        zipCode: number;
+        occupation: String;
+   }
